b73d7dc0d10fd0568b9f838430a69c21926190eb
herlev <>
2024-09-30 14:03:20 +0200

    refactor client

--- src/client.rs
+++ src/client.rs
@@ -21,13 +21,9 @@ fn decode_msg(buf: &[u8]) -> Option<Msg> {
   Some(msg)
 }
 
-pub fn client(server_endpoint: SocketAddr, server_forward_port: u16, forward_to_endpoint: SocketAddr) {
-  let mut hosts = HashMap::<SocketAddr, UdpSocket>::new();
+fn server_connect(server_endpoint: SocketAddr, server_forward_port: u16) -> UdpSocket {
   let config = bincode::DefaultOptions::new().with_fixint_encoding();
-  let server_socket = UdpSocket::bind("0.0.0.0:0").unwrap(); // automatically get port from OS
-  debug!("using {server_socket:?}");
-
-  debug!("using {server_endpoint:?}");
+  let server_socket = UdpSocket::bind("0.0.0.0:0").unwrap();
   server_socket.connect(server_endpoint).unwrap();
   server_socket
     .set_read_timeout(Some(Duration::from_millis(500)))
@@ -49,36 +45,77 @@ pub fn client(server_endpoint: SocketAddr, server_forward_port: u16, forward_to_
     Command::Response(Response::Success) => info!("Server successfully openend port {server_forward_port}"),
     Command::Response(Response::PortInUse) => {
       error!("Requested server port is already in use");
-      return;
+      panic!("");
     }
     cmd => {
       error!("Received invalid response from server {cmd:?}");
-      return;
+      panic!("");
     }
   }
+  server_socket.set_read_timeout(None).unwrap();
+  server_socket
+}
 
-  let tx_socket = server_socket.try_clone().unwrap();
-  let (s, r) = mpsc::channel::<Msg>();
+fn spawn_message_forwarder(tx_socket: UdpSocket, msg_channel: mpsc::Receiver<Msg>) {
+  let config = bincode::DefaultOptions::new().with_fixint_encoding();
   std::thread::spawn(move || loop {
-    let msg = r.recv().unwrap();
+    let msg = msg_channel.recv().unwrap();
     let bytes = config.serialize(&msg).unwrap();
     tx_socket.send(&bytes).unwrap();
   });
+}
 
-  let keepalive_sender = s.clone();
+fn spawn_keepalive_sender(msg_channel: mpsc::Sender<Msg>) {
   std::thread::spawn(move || loop {
     std::thread::sleep(KEEPALIVE_DURATION);
     debug!("Sending keepalive");
-    keepalive_sender.send(Msg::new(Command::Keepalive)).unwrap();
+    msg_channel.send(Msg::new(Command::Keepalive)).unwrap();
+  });
+}
+
+fn receive_msg(rx_socket: &UdpSocket) -> Msg {
+  let mut buf = [0; 2048];
+  let n = rx_socket.recv(&mut buf).unwrap();
+  assert!(n < 2048, "increase buffer size");
+  let msg = decode_msg(&buf[0..n]).unwrap();
+  msg
+}
+
+fn create_client(
+  hosts: &mut HashMap<SocketAddr, UdpSocket>,
+  client_socket: SocketAddr,
+  msg_channel: mpsc::Sender<Msg>,
+  forward_to_endpoint: SocketAddr,
+) -> &UdpSocket {
+  let rx_socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+  rx_socket.connect(forward_to_endpoint).unwrap();
+  let tx_socket = rx_socket.try_clone().unwrap();
+
+  std::thread::spawn(move || {
+    let mut buf = [0; 2048];
+    loop {
+      let n = rx_socket.recv(&mut buf).unwrap();
+      assert!(n < 2048, "increase buffer size");
+      msg_channel
+        .send(Msg::new(Command::UdpPayload(client_socket, buf[0..n].to_vec())))
+        .unwrap();
+    }
   });
 
-  let mut buf = [0; 2048];
-  server_socket.set_read_timeout(None).unwrap();
-  loop {
-    let n = server_socket.recv(&mut buf).unwrap();
-    assert!(n < 2048, "increase buffer size");
-    let msg = decode_msg(&buf[0..n]).unwrap();
+  hosts.insert(client_socket, tx_socket);
+  hosts.get(&client_socket).unwrap()
+}
 
+pub fn client(server_endpoint: SocketAddr, server_forward_port: u16, forward_to_endpoint: SocketAddr) {
+  let mut hosts = HashMap::<SocketAddr, UdpSocket>::new();
+  let server_socket = server_connect(server_endpoint, server_forward_port);
+
+  let (send_msg_channel, recv_msg_channel) = mpsc::channel::<Msg>();
+  spawn_message_forwarder(server_socket.try_clone().unwrap(), recv_msg_channel);
+  spawn_keepalive_sender(send_msg_channel.clone());
+
+  loop {
+    let msg = receive_msg(&server_socket);
     let (src, payload) = match msg.command {
       Command::UdpPayload(src, payload) => (src, payload),
       cmd => {
@@ -90,26 +127,8 @@ pub fn client(server_endpoint: SocketAddr, server_forward_port: u16, forward_to_
     let socket = if let Some(socket) = hosts.get(&src) {
       socket
     } else {
-      let rx_socket = UdpSocket::bind("127.0.0.1:0").unwrap();
-      // rx_socket.connect(forward_to_endpoint).unwrap();
-      let tx_socket = rx_socket.try_clone().unwrap();
-      let s = s.clone();
-      std::thread::spawn(move || {
-        let mut buf = [0; 2048];
-        loop {
-          let (n, src2) = rx_socket.recv_from(&mut buf).unwrap();
-          assert!(src2 == forward_to_endpoint);
-          assert!(n < 2048, "increase buffer size");
-          s.send(Msg::new(Command::UdpPayload(src, buf[0..n].to_vec()))).unwrap();
-        }
-      });
-      hosts.insert(src, tx_socket);
-      hosts.get(&src).unwrap()
+      create_client(&mut hosts, src, send_msg_channel.clone(), forward_to_endpoint)
     };
-
-    // let socket = hosts
-    //   .entry(src)
-    //   .or_insert_with(|| UdpSocket::bind("127.0.0.1:0").unwrap());
 
     info!("forwarding {payload:?} to {forward_to_endpoint:?} using {socket:?}");
     socket.send_to(&payload, forward_to_endpoint).unwrap();
